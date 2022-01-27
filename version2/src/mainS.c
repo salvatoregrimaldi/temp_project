bcdbfcd1c43587e7033410cd82fba9eac44b27ce
edimauro5 <e.dimauro5@studenti.unisa.it>
2022-01-27 14:28:57 +0100

    misurazioni corrette

--- version2/src/mainS.c
+++ version2/src/mainS.c
@@ -82,7 +82,7 @@ int main(int argc, char *argv[])
 
     ENDTIME(1, time_count);
 
-    readingFile(file_name, n);
+    //readingFile(file_name, n);
 
     printf("1;%f;%f\n", time_init, time_count);
 }
